e299745dd699c8003732afc0e02652201194d2ee
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-09-08 12:15:39 +1000

    OSM-4: Added antd for UI components + beginning to add mapping features

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,38 @@
-import React from 'react';
-import logo from './images/logo.svg';
+import React, {useState, useEffect} from 'react';
+import {Button} from 'antd';
 import './css/App.css';
+import { OLMap } from './components/OLMap';
+
+// function FileUploader() {
+//   let htmlData;
+//   const defaultFileType = "html";
+//   let fileInput = React.createRef();
+//   let fileInput2 = React.createRef();
+
+//   const [selectFile, setFile] = useState({
+//     fileType: defaultFileType,
+//     fileDownloaderURL: null,
+//     status: "",
+//     newFileName: "",
+//     data: "",
+//   });
+
 
 function App() {
   return (
     <div className="App">
       <header className="tool-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <h1>
+          Mapping Pro
+        </h1>
+        <Button type="primary" >Upload</Button>
+
   
       </header>
       <section className="work-section">
 
         <div className="split map-area">
+         
 
         </div>
         <div className="split toolset">
