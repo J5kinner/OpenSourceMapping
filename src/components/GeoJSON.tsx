10c46ca291149909db26179f3e6d58f38e3fcf3c
Jonah Skinner <jonah.lee.skinner@gmail.com>
2022-10-05 15:56:41 +1100

    First attempt at making an upload button

--- src/components/GeoJSON.tsx
+++ src/components/GeoJSON.tsx
@@ -7,8 +7,25 @@ import { RMap, ROSM, RLayerVector, RStyle } from "rlayers";
 
 import geojsonFeatures from "./data/geo.json";
 
+import FileUploadButton from "./FileUploadButton";
+
 export default function Features(): JSX.Element {
   const [flow, setFlow] = React.useState([]);
+  const [fileSelected, setFileSelected] = React.useState<File>() // also tried <string | Blob>
+  const handleFileChange = function (e: React.ChangeEvent<HTMLInputElement>) {
+    const fileList = e.target.files;
+
+    if (!fileList) return;
+
+    setFileSelected(fileList[0]);
+};
+
+    const uploadFile = function (e: React.MouseEvent<HTMLSpanElement, MouseEvent>) {
+        if (fileSelected) {
+            const formData = new FormData();
+            formData.append("data", fileSelected, fileSelected.name);
+        }
+    };
   return (
     <div className="d-flex flex-row">
       <RMap
@@ -16,6 +33,7 @@ export default function Features(): JSX.Element {
         initial={{ center: fromLonLat([2.364, 48.82]), zoom: 11 }}
         height={"300px"}
       >
+       <input type="file" onChange={ (e) => this.handleFileChange(e.target.files) } />
         <ROSM />
         {/* From a static file included at bundling time */}
         {/* (this won't work in CodePen) */}
@@ -38,7 +56,7 @@ export default function Features(): JSX.Element {
           </RStyle.RStyle>
         </RLayerVector>
         {/* From an URL */}
-        <RLayerVector
+        {/* <RLayerVector
           zIndex={5}
           format={new GeoJSON({ featureProjection: "EPSG:3857" })}
           url="https://raw.githubusercontent.com/gregoiredavid/france-geojson/master/departements.geojson"
@@ -53,7 +71,7 @@ export default function Features(): JSX.Element {
             <RStyle.RStroke color="#007bff" width={3} />
             <RStyle.RFill color="transparent" />
           </RStyle.RStyle>
-        </RLayerVector>
+        </RLayerVector> */}
       </RMap>
       <div className="mx-0 mt-0 mb-3 p-1 w-100 jumbotron shadow example-list">
         <p>Your actions</p>
